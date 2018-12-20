216104ca2f9c3a1412e039a62be6d35a40be61b4
antalmega <kikoalmelagarcia@gmail.com>
2018-12-20 13:47:52 +0100

    config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
-// webpack 4
-
 const path = require('path');
-// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   entry: { index: './src/js/index.js' },
@@ -16,34 +15,59 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [{ loader: 'html-loader', options: { minimize: true } }]
+      },
+      {
+        test: /\.(png|jp?g|svg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: './img/[name].[ext]',
+              limit: 10000
+            }
+          },
+          {
+            loader: 'img-loader'
+          }
+        ]
       },
       {
         test: /\.scss$/,
         exclude: /node_modules/,
         use: [
-          'style-loader',
+          // 'style-loader',
           MiniCssExtractPlugin.loader,
           'css-loader',
+          'postcss-loader',
           'sass-loader'
         ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader']
       }
+      // {
+      //   test: /\.(png|svg|jpg|gif)$/,
+      //   use: ['file-loader']
+      // }
     ]
   },
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
+  },
   plugins: [
-    // new ExtractTextPlugin({ filename: './css/style.css' }),
-    new MiniCssExtractPlugin({
-      filename: '[name].bundle.css'
-    }),
     new HtmlWebpackPlugin({
       // inject: false,
-      hash: true,
+      // hash: true,
       template: './src/index.html',
       filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].bundle.css',
+      chunkFilename: '[id].css'
     })
   ]
 };
