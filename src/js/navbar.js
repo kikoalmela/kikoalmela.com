b296ef601de5687275c5c5dcdc431900ecf4fa33
antalmega <kikoalmelagarcia@gmail.com>
2019-01-11 19:35:53 +0100

    refactor - split index.js

---
+++ src/js/navbar.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const menuOpen = document.getElementById('navi-toggle'); // Checkbox that handle collapsed menu
+const menuItems = document.querySelectorAll('.navbar__link');
+const navLogo = document.querySelector('.navbar__logo-img');
+
+const sections = document.querySelectorAll('section');
+const navHeight = document.getElementById('navbar').offsetHeight;
+const headerHeight = document.querySelector('header').offsetHeight;
+
+const closeMenu = () => (menuOpen.checked = false);
+
+// Close menu on clicking item
+const HandleClickItem = () =>
+  menuItems.forEach(el => el.addEventListener('click', closeMenu));
+
+// Close menu on clicking logo
+const HandleClickLogo = () =>
+  navLogo.addEventListener('click', () => {
+    if (checkbox.checked) closeMenu();
+  });
+
+//  Set active menu item on scroll
+const activeMenuItemOnScroll = () => {
+  const currentPos = window.scrollY;
+  sections.forEach(section => {
+    const top = section.offsetTop - navHeight;
+    const bottom = top + section.offsetHeight;
+    const href = `#${section.getAttribute('id')}`;
+
+    if (currentPos >= top && currentPos <= bottom) {
+      menuItems.forEach(item => {
+        const itemHref = item.getAttribute('href');
+        itemHref === href
+          ? item.classList.add('active')
+          : item.classList.remove('active');
+      });
+    } else if (currentPos < headerHeight - navHeight) {
+      menuItems.forEach(item => item.classList.remove('active'));
+    }
+  });
+};
+
+// Highlight menu item related to url hash
+// Less consuming than activeMenuItemOnScroll
+// but doesn't update active item on scroll
+function activeMenuItemByUrlHash() {
+  let hash = window.location.hash;
+
+  menuItems.forEach(el => {
+    let elHref = el.getAttribute('href');
+    elHref === hash
+      ? el.classList.add('active')
+      : el.classList.remove('active');
+  });
+}
+
+const handleMenuItemsHover = () => {
+  menuItems.forEach(link => {
+    link.addEventListener('mouseenter', function() {
+      menuItems.forEach(item => item.classList.remove('active'));
+    });
+  });
+
+  menuItems.forEach(link => {
+    link.addEventListener('mouseleave', activeMenuItemOnScroll);
+  });
+};
+
+const lessLoadActiveMenu = false;
+
+const handleNav = () => {
+  window.onscroll = lessLoadActiveMenu
+    ? activeMenuItemByUrlHash
+    : activeMenuItemOnScroll;
+  HandleClickItem();
+  HandleClickLogo();
+  handleMenuItemsHover();
+};
+
+export default handleNav;
---
+++ src/js/utils.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// sizing the actual viewport height non cropped on mobile browsers
+export const setVhVariable = () => {
+  // get the viewport height and multiple it by 1% to get a value for a vh unit
+  let vh = window.innerHeight * 0.01;
+  // set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+  ///////////////////
+  // --vh update on window resize.
+  // Causes weird behavior in Safari iOS due to bars auto hidden on scroll.
+  // window.addEventListener("resize", setVhVariable);
+};
+
+export const setCopyright = () => {
+  // Get current year
+  const year = new Date().getFullYear();
+  // Set copyright text
+  const copyright = document.querySelector('.copy');
+  copyright.innerHTML = `&copy; ${year} Kiko Almela`;
+};
