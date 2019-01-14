24fdaa6763ad0fcc735c1708b74d902819575bf8
antalmega <kikoalmelagarcia@gmail.com>
2019-01-14 14:52:14 +0100

    config initcookieconsent

--- src/js/initCookieconsent.js
+++ src/js/initCookieconsent.js
@@ -4,12 +4,66 @@ const initCookieconsent = () => {
   window.cookieconsent.initialise({
     container: document.querySelector('.container'),
     palette: {
-      popup: { background: '#fff' },
-      button: { background: '#aa0000' }
+      popup: { background: '#292a5b', text: '#FFFCF2', link: '#FFE173' },
+      button: { background: '#FFE173', text: '#292a5b' },
+      highlight: {
+        background: 'transparent',
+        border: '#FFE173',
+        text: '#FFE173'
+      }
     },
+    cookie: { domain: '.kikoalmela.com' },
+    content: {
+      header: '¡Este sitio utiliza cookies!',
+      message:
+        'Este sitio utiliza cookies propias y de terceros para obtener datos estadísticos de la navegación de los usuarios y mejorar esta página web. Si aceptas se considera que permites su uso. Puedes cambiar la configuración u obtener más información',
+      dismiss: 'Entendido',
+      allow: 'Aceptar',
+      deny: 'Rechazar',
+      link: 'aquí',
+      href: 'https://www.kikoalmela.com/rgpd/politica-de-cookies',
+      close: '&#x274c;',
+      policy: 'Configuración de Cookies',
+      target: '_blank'
+    },
+    type: 'opt-in',
     revokable: true,
-    onStatusChange: function(status) {
-      console.log(this.hasConsented() ? 'enable cookies' : 'disable cookies');
+    onInitialise: function(status) {
+      var type = this.options.type;
+      var didConsent = this.hasConsented();
+      if (type == 'opt-in' && didConsent) {
+        // enable cookies
+        console.log('cookies aceptadas');
+      }
+      if (type == 'opt-out' && !didConsent) {
+        // disable cookies
+        console.log('cookies rechazadas');
+      }
+    },
+
+    onStatusChange: function(status, chosenBefore) {
+      var type = this.options.type;
+      var didConsent = this.hasConsented();
+      if (type == 'opt-in' && didConsent) {
+        // enable cookies
+        console.log('cookies aceptadas');
+      }
+      if (type == 'opt-out' && !didConsent) {
+        // disable cookies
+        console.log('cookies rechazadas');
+      }
+    },
+
+    onRevokeChoice: function() {
+      var type = this.options.type;
+      if (type == 'opt-in') {
+        // disable cookies
+        console.log('cookies rechazadas');
+      }
+      if (type == 'opt-out') {
+        // enable cookies
+        console.log('cookies aceptadas');
+      }
     },
     law: {
       regionalLaw: false
