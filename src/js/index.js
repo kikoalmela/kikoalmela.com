9cc4d2f7c576af250d37f0c97674e65ccf7bdd6b
antalmega <kikoalmelagarcia@gmail.com>
2019-01-22 12:35:25 +0100

    remove cookieconsent

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,8 @@
 import '../sass/main.scss';
-import './lib/cookieconsent/cookieconsent.min.css';
 import handleNav from './navbar';
 import { setVhVariable, setCopyright } from './utils';
-import cookieconsent from './lib/cookieconsent/cookieconsent';
-import initCookieconsent from './initCookieconsent';
 
 const loadScripts = () => {
-  initCookieconsent();
   handleNav();
   setVhVariable();
   setCopyright();
